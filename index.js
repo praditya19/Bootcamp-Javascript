5b4e294caf276551832f1a08a2561f89b7f2ccb3
praditya <praditya.rendi88@gmail.com>
2019-01-08 09:08:13 +0700

    Functions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-let selectedColors = ['green', 'white', 'red'];
-selectedColors[7] = 1;
-console.log(selectedColors.length);
+function binus(name, lastName) {
+  console.log('Hay ' + name + ' ' + lastName);
+}
+
+binus('praditya', 'rendi!!');
