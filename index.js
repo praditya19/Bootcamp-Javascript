9484d831453328309fa7eaafc0b92029d554d697
praditya <praditya.rendi88@gmail.com>
2019-01-10 16:23:35 +0700

    Comparison Operators

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-let selectedColor = ['red', 'blue'];
-selectedColor[2] = 1;
-console.log(selectedColor.length);
+let x = 2;
+
+//hubungan
+console.log(x > 0);
+console.log(x >= 2);
+console.log(x < 2);
+console.log(x <= 2);
+
+//persamaan
+console.log(x === 2);
+console.log(x !== 2);
