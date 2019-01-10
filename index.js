e6d36cd0074c1ef1185a049c3ba5f1b250011655
praditya <praditya.rendi88@gmail.com>
2019-01-10 17:05:29 +0700

    Operators Precedence

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// Read, Write, Execute
-//00000100
-//00000010
-//00000001
+let rumus = (10 + 50) * 100;
 
-const readPermission = 4;
-const writePermission = 2;
-const executePermission = 1;
-
-let myPermission = 0;
-
-let message = myPermission & readPermission ? 'yes' : 'no';
-
-console.log(message);
+console.log(rumus);
