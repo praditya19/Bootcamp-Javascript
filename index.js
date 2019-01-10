4af4539d488d65adc2f35bbffa3457a3b9a8857c
praditya <praditya.rendi88@gmail.com>
2019-01-10 21:16:57 +0700

    Switch...case

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-let hour = 20;
+let role = 'guest';
 
-if (hour >= 6 && hour < 12) console.log('Good morning');
-else if (hour >= 12 && hour < 20) console.log('Good afternoon');
-else console.log('Good evening');
+switch (role) {
+  case 'guest':
+    console.log('Guest User');
+    break;
+
+  case 'moderator':
+    console.log('Moderator User');
+    break;
+
+  default:
+    console.log('Unknown User');
+}
+
+if (role === 'guest') console.log('Guest');
+else if (role === 'moderator') console.log('Moderator');
+else console.log('Unknown User');
