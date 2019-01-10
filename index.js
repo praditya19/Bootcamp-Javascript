13ea3968cbd6886961890f8cc06962ca0f30fb2e
praditya <praditya.rendi88@gmail.com>
2019-01-10 21:43:47 +0700

    Do...while

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-for (let i = 0; i <= 5; i++) {
-  if (i % 2 !== 0) console.log(i);
-}
-
-let i = 0;
-while (i <= 5) {
+let i = 9;
+do {
   if (i % 2 !== 0) console.log(i);
   i++;
-}
+} while (i <= 5);
