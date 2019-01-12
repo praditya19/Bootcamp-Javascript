b5427acc20bb9bbd41d929666168516a77c5a2ac
praditya <praditya.rendi88@gmail.com>
2019-01-12 20:32:32 +0700

    For...of

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const person = {
 
 for (let key in person) console.log(key, person[key]);
 
-const colors = ['yellow', 'red', 'blue'];
+const colors = ['yellow', 'red', 'blue', 'green'];
 
-for (let index in colors) console.log(index, colors[index]);
+for (let color of colors) console.log(color);
