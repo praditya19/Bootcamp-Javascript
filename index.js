a81e5d2806b364ba7d8982199e77a3800af68b66
praditya <praditya.rendi88@gmail.com>
2019-01-12 21:48:41 +0700

    Exercise 6- Count Truthy

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-showNumbers(20);
+const array = [0, null, undefined, '', 5, 7];
 
-function showNumbers(limit) {
-  for (let i = 0; i <= limit; i++) {
-    const message = i % 2 === 0 ? 'EVEN' : 'ODD';
-    console.log(i, message);
-  }
+console.log(countTruthy(array));
+
+function countTruthy(array) {
+  let count = 0;
+  for (let value of array) if (value) count++;
+  return count;
 }
