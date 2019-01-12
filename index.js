99278ce40f20d9c59e41073df7ff4d6862ba319d
praditya <praditya.rendi88@gmail.com>
2019-01-12 22:11:20 +0700

    Exercise 9- Grade

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-console.log(sum(15));
+function binus123(marks) {
+  const average = binusverage(marks);
+  if (average < 60) return 'f';
+  if (average < 70) return 'D';
+  if (average < 80) return 'C';
+  if (average < 90) return 'B';
+  return 'A';
+}
 
-function sum(limit) {
+function binusverage(array) {
   let sum = 0;
-  for (let i = 0; i <= limit; i++) if (i % 3 === 0 || i % 5 === 0) sum += i;
-  return sum;
+  for (let value of array) sum += value;
+  return sum / array.length;
 }
