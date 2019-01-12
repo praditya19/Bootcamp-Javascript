23123be2e515c8d344348eb6507b01688348370b
praditya <praditya.rendi88@gmail.com>
2019-01-12 21:42:41 +0700

    Exercise 5- Even and Odd Numbers

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-checkSpeed(130);
+showNumbers(20);
 
-function checkSpeed(speed) {
-  const speedLimit = 70;
-  const kmPerPoint = 5;
-
-  if (speed < speedLimit + kmPerPoint) console.log('ok');
-  else {
-    const points = Math.floor((speed - speedLimit) / kmPerPoint);
-    if (points >= 12) console.log('Licenci Berhasil');
-    else console.log('points', points);
+function showNumbers(limit) {
+  for (let i = 0; i <= limit; i++) {
+    const message = i % 2 === 0 ? 'EVEN' : 'ODD';
+    console.log(i, message);
   }
 }
