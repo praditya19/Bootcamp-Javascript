545d9535eca26e0c0f0a5d1f6b9ece5f0f66580d
praditya <praditya.rendi88@gmail.com>
2019-01-12 21:33:17 +0700

    Exercise 4- Demerit Points

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const output = fizzBuzz(false);
-console.log(output);
+checkSpeed(130);
 
-function fizzBuzz(input) {
-  if (typeof input !== 'number') return NaN;
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
 
-  if (input % 3 === 0 && input % 5 === 0) return 'fizz';
-
-  if (input % 5 === 0) return 'Buzz';
-
-  return input;
+  if (speed < speedLimit + kmPerPoint) console.log('ok');
+  else {
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points >= 12) console.log('Licenci Berhasil');
+    else console.log('points', points);
+  }
 }
