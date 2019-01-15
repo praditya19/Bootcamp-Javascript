7badb963c2d73c4da08ac840f12be77bdde60d0d
praditya <praditya.rendi88@gmail.com>
2019-01-15 20:35:27 +0700

    Constructor Functions

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ function createCircle(radius) {
   };
 }
 
-const circle1 = createCircle(1);
-console.log(circle1);
+const myCircle1 = createCircle(1);
 
-const circle2 = createCircle(2);
-console.log(circle2);
+//Constructor Function
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log('draw');
+  };
+}
