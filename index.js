8ca6b27a6cf15d6a76f85c49f2843a24fc20dc29
praditya <praditya.rendi88@gmail.com>
2019-01-15 20:29:15 +0700

    Factory Functions

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1
-  },
-  isVisible: true,
-  draw: function() {
-    console.log('draw');
-  }
-};
+function createCircle(radius) {
+  return {
+    radius,
+    draw() {
+      console.log('draw');
+    }
+  };
+}
 
-circle.draw();
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2);
