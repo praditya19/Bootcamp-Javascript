f22c1e5070c177d38554c87f6d4a60b66bab2129
praditya <praditya.rendi88@gmail.com>
2019-01-15 21:53:47 +0700

    Garbage Collection

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-const circle = {
-  radius: 1,
-  draw() {
-    console.log('draw');
-  }
-};
-
-//const another = {};
-//for (let key in circle);
-//another[key] = circle[key];
-
-const another = Object.assign({}, circle);
-
-const another = { ...circle };
-
-console.log(another);
+let circle = {};
+console.log(circle);
