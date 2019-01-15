abb6e550c829ef4681789ec3e6c3e92f5d2b2e72
praditya <praditya.rendi88@gmail.com>
2019-01-15 21:27:26 +0700

    Enumerating Properties of an Object

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-let obj = { value: 20 };
+const circle = {
+  radius: 1,
+  draw() {
+    console.log('draw');
+  }
+};
 
-function increase(obj) {
-  obj.value++;
-}
+for (let key in circle) console.log(key, circle[key]);
 
-increase(obj);
-console.log(obj);
+for (let key of Object.keys(circle)) console.log(key);
+
+for (let entry of Object.entries(circle)) console.log(entry);
+
+if ('color' in circle) console.log('yes');
