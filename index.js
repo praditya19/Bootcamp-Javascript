34685796ae3ecfc4ade3135befaf889c0a14f051
praditya <praditya.rendi88@gmail.com>
2019-01-15 07:50:44 +0700

    Exercise- Prime Numbers

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-showStars(2);
+showPrimes(10);
 
-function showStars(rows) {
-  for (let row = 1; row <= rows; row++) {
-    let pattern = '';
-    for (let i = 0; i < row; i++) pattern += '*';
-    console.log(pattern);
-  }
+function showPrimes(limit) {
+  for (let number = 2; number <= limit; number++)
+    if (isPrime(number)) console.log(number);
+}
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++)
+    if (number % factor === 0) return false;
+
+  return true;
 }
