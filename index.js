bce48b20c19775aeaf82d599974b33949f97e3eb
praditya <praditya.rendi88@gmail.com>
2019-01-16 19:22:52 +0700

    Exercise 3- Object Equality

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-let address = {
-  street: 'a',
-  city: 'b',
-  zipCode: 'c'
-};
+let address1 = new Address('a', 'b', 'c');
+let address2 = new Address('a', 'b', 'c');
+let address3 = address1;
 
-function showAddresse(address) {
-  for (let key in address) console.log(key, address[key]);
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
+console.log(areSame(address1, address3));
+
+//Construction Function
+function Address(street, city, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
 }
-showAddresse(address);
+
+function areEqual(address1, address2) {
+  return (
+    address1.street === address2.street &&
+    address1.city === address2.city &&
+    address1.zipCode === address2.zipCode
+  );
+}
+
+function areSame(address1, address2) {
+  return address1 === address2;
+}
