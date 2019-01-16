8e7f5d50d215b9d825a5e551399157f2497f2f6f
praditya <praditya.rendi88@gmail.com>
2019-01-16 11:08:36 +0700

    Date

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const name = 'rendi';
-const message = 'hi ' + name + ',\n';
+const now = new Date();
+const date1 = new Date('january 16 2018 11.00');
+const date2 = new Date(2018, 4, 11, 9);
 
-const another = `hi ${name} ${2 + 3}
-Thank you for joining my mailing list
-
-Regards
-rendi
-`;
+now.setFullYear(2017);
