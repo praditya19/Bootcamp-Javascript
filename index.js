2abb92e469987578a54247d47b637f9b9f2e9670
praditya <praditya.rendi88@gmail.com>
2019-01-16 20:04:01 +0700

    Arrow Functions

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const courses = [{ id: 1, name: 'a' }, { id: 2, name: 'b' }];
 
-const course = courses.findIndex(function(course) {
-  return course.name === 'a';
-});
+const course = courses.find(course => course.name === 'a');
 
 console.log(course);
