9bd673803e177346e084071e49e015eeff6b0928
praditya <praditya.rendi88@gmail.com>
2019-01-16 19:30:30 +0700

    Exercise 4- Blog Post Object

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-let address1 = new Address('a', 'b', 'c');
-let address2 = new Address('a', 'b', 'c');
-let address3 = address1;
+let post = {
+  title: 'a',
+  body: 'b',
+  author: 'c',
+  views: 10,
+  comment: [{ author: 'a', body: 'b' }, { author: 'c', body: 'd' }],
+  isLive: true
+};
 
-console.log(areEqual(address1, address2));
-console.log(areSame(address1, address2));
-console.log(areSame(address1, address3));
-
-//Construction Function
-function Address(street, city, zipCode) {
-  this.street = street;
-  this.city = city;
-  this.zipCode = zipCode;
-}
-
-function areEqual(address1, address2) {
-  return (
-    address1.street === address2.street &&
-    address1.city === address2.city &&
-    address1.zipCode === address2.zipCode
-  );
-}
-
-function areSame(address1, address2) {
-  return address1 === address2;
-}
+console.log(post);
