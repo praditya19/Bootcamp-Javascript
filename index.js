6119a22d99172ea2146bde026509790ded1e5938
praditya <praditya.rendi88@gmail.com>
2019-01-16 20:00:13 +0700

    Finding Elements (Reference Types)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const numbers = [1, 2, 3, 1, 4];
+const courses = [{ id: 1, name: 'a' }, { id: 2, name: 'b' }];
 
-console.log(numbers.indexOf(1, 2));
-console.log(numbers.lastIndexOf(1));
+const course = courses.findIndex(function(course) {
+  return course.name === 'a';
+});
 
-console.log(numbers.indexOf(1) !== -1);
-console.log(numbers.includes(1));
+console.log(course);
