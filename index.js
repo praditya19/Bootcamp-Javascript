631165f63e0b15307b94df0a87f71f6fd775b27a
praditya <praditya.rendi88@gmail.com>
2019-01-16 11:33:53 +0700

    Exercise 1- Address Object

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const now = new Date();
-const date1 = new Date('january 16 2018 11.00');
-const date2 = new Date(2018, 4, 11, 9);
+let address = {
+  street: 'a',
+  city: 'b',
+  zipCode: 'c'
+};
 
-now.setFullYear(2017);
+function showAddress(address) {
+  for (let key in address) console.log(key, address[key]);
+}
+
+showAddress(address);
