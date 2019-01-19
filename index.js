6f48d858a38e8b2e15c0851221892082f57ba70a
Praditya Rendi 28 <praditya.rendi88@gmail.com>
2019-01-19 20:41:00 +0700

    Iterating an Array

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const first = [1, 2, 3];
-const second = [4, 5, 6];
+const numbers = [1, 2, 3];
 
-//const combinasi = first.concat(second);
-const combinasi = [...first, 'a', ...second, 'b'];
+for (let number of numbers) console.log(number);
 
-//const copy = combinasi.slice();
-const copy = [combinasi];
+numbers.forEach((number, index) => console.log(index, number));
