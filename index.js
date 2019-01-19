25051eca938697e8669d315f5e58d847d5bd9e20
Praditya Rendi 28 <praditya.rendi88@gmail.com>
2019-01-19 21:03:08 +0700

    Testing the Elements of an Array

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-const choice = [
-  { id: 1, name: 'node.js' },
-  { id: 2, name: 'javaScript' },
-  { id: 2, name: 'React Js' },
-  { id: 2, name: 'React Native' }
-];
+const number = [1, -1, 2, 3];
 
-choice.sort(function(a, b) {
-  const nameA = a.name.toLowerCase();
-  const nameB = b.name.toLowerCase();
-
-  if (nameA < nameB) return -1;
-  if (nameA > nameB) return 1;
-  return 0;
+const atLeastOnePositive = number.some(function(value) {
+  return value >= 0;
 });
 
-console.log(choice);
+console.log(atLeastOnePositive);
