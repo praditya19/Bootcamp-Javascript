b3bb2453607761f1f5d3af3f43d9a25a6f0fc2c0
Praditya Rendi 28 <praditya.rendi88@gmail.com>
2019-01-19 22:23:37 +0700

    Exercise 7- Movies

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const number = [1, 2, 3, 4];
+const movies = [
+  { title: 'a', year: 2018, rating: 4.5 },
+  { title: 'b', year: 2018, rating: 4.7 },
+  { title: 'c', year: 2018, rating: 3 },
+  { title: 'd', year: 2017, rating: 4.5 }
+];
 
-const max = getMax([1, 2, 2, 1, 3]);
+const titles = movies
+  .filter(m => m.year === 2018 && m.rating >= 4)
+  .sort((a, b) => a.rating - b.rating)
+  .reverse()
+  .map(m => m.title);
 
-console.log(max);
-
-function getMax(array) {
-  if (array.lenght === 0) return undefined;
-
-  return array.reduce((a, b) => (a > b ? a : b));
-}
+console.log(titles);
