ded9836581f9844f1e9dc2ffef8c06961c657334
Praditya Rendi 28 <praditya.rendi88@gmail.com>
2019-01-20 20:53:24 +0700

    Exercise 1- Sum of Arguments

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const video = {
-  title: 'a',
-  tags: ['a', 'b', 'c'],
-  showTags() {
-    this.tags.forEach(tag => {
-      console.log(this.title, tag);
-    });
-  }
-};
+console.log(sum(1, 2, 3, 4));
 
-video.showTags();
+function sum(...items) {
+  if (items.lenght === 1 && Array.isArray(items[0])) items = [...items[0]];
+
+  return items.reduce((a, b) => a + b);
+}
