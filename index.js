40ee7ce864b1bdbe16f91fbf782e25f6981aa258
Praditya Rendi 28 <praditya.rendi88@gmail.com>
2019-01-20 21:06:32 +0700

    Exercise 3- Error Handling

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const circle = {
-  radius: 1,
-  get area() {
-    return Math.PI * this.radius * this.radius;
-  }
-};
+try {
+  const numbers = [1, 2, 3, 4];
+  const count = countOccurrances(null, 1);
+  console.log(count);
+} catch (e) {
+  console.log(e.message);
+}
 
-console.log(circle.area);
+function countOccurrances(array, searchElement) {
+  if (!Array.isArray(array)) throw new Error('Invalid Array.');
+
+  return array.reduce((accumulator, current) => {
+    const occurrance = current === searchElement ? 1 : 0;
+    return accumulator + occurrance;
+  }, 0);
+}
